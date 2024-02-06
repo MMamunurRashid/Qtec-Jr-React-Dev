22faf000b408d9c7efe06784e5bee13883d24f19
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2024-02-06 20:11:55 +0600

    new task store in local storage

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,25 @@
 "use client";
+import { useState } from 'react';
 import TaskForm from '../components/TaskForm/TaskForm';
+
 export default function Home() {
-  
+  const [tasks, setTasks] = useState([]);
+
+  // add new task
+  const addTask = (newTask) => {
+    const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const updatedTasks = [...existingTasks, newTask];
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-serif font-bold text-center pt-5 pb-3 shadow-lg">ToDo List App</h1>
-    {/* task form  */}
-    <TaskForm />
+      {/* TaskForm  */}
+      <TaskForm onAddTask={addTask} />
+      
     </div>
   );
 }
--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,11 +1,39 @@
-const TaskForm = () => {
+import { useState } from 'react';
+
+const TaskForm = ({ onAddTask }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const title = e.target.title.value;
     const description = e.target.description.value;
     const priority = e.target.priority.value;
-    console.log(title, description, priority);
+
+    const options = {
+        timeZone: 'Asia/Dhaka',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      };
+      const bangladeshiDateTime = new Date().toLocaleString('en-US', options);
+  
+    
+
+    const newTask = {
+      id: Math.floor(Math.random() * 1000), 
+      date: bangladeshiDateTime,
+      title,
+      description,
+      priority,
+      completed: false 
+    };
+
+   
+    onAddTask(newTask);
+
+    e.target.reset();
   };
 
   return (
@@ -14,43 +42,41 @@ const TaskForm = () => {
         Add a New Task
       </h1>
 
-      
-        <label>Add Task Title</label>
-        <input
-          type="text"
-          name="title"
-          placeholder="Add a new task title..."
-          className="border border-gray-200 px-4 py-2 mb-2 rounded-lg  w-full"
-        />
-   
-    
-        <label>Add Task Description</label>
-        <input
-          type="text"
-          name="description"
-          placeholder="Add a new task description..."
-          className="border border-gray-200 px-4 py-2 mb-2 rounded-lg  w-full"
-        />
+      <label>Add Task Title</label>
+      <input
+        type="text"
+        name="title"
+        required
+        placeholder="Add a new task title..."
+        className="border border-gray-200 px-4 py-2 mb-2 rounded-lg  w-full"
+      />
+  
+      <label>Add Task Description</label>
+      <textarea
+        type="text"
+        name="description"
+        required
+        placeholder="Add a new task description..."
+        className="border border-gray-200 px-4 py-2 mb-2 rounded-lg  w-full h-[80px]"
+      />
 
-   
-        <label>Add Task Priority</label>
-        <select
-          name="priority"
-          className="border border-gray-200 px-4 py-2 mb-2 rounded-lg  w-full"
-        >
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
-        </select>
-     
+      <label>Add Task Priority</label>
+      <select
+        name="priority"
+        className="border border-gray-200 px-4 py-2 mb-2 rounded-lg  w-full"
+      >
+        <option value="high">High</option>
+        <option value="medium">Medium</option>
+        <option value="low">Low</option>
+      </select>
 
       <div className="flex justify-center">
-      <button
-        type="submit"
-        className="ml-2 bg-blue-500 text-white py-2 px-16 rounded-lg w-1/2"
-      >
-        Add Task
-      </button>
+        <button
+          type="submit"
+          className="ml-2 bg-blue-500 text-white py-2 px-16 rounded-lg w-1/2"
+        >
+          Add Task
+        </button>
       </div>
     </form>
   );
