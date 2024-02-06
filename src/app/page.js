ca405424d36da88314f26329dc3a9d1cde01599b
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2024-02-06 22:08:12 +0600

    edit delete complete button functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,91 @@
 "use client";
-import { useState, useEffect } from 'react';
-import TaskForm from '../components/TaskForm/TaskForm';
-import Task from '../components/Task/Task';
+import { useState, useEffect } from "react";
+import TaskForm from "../components/TaskForm/TaskForm";
+import Task from "../components/Task/Task";
 
 export default function Home() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
     setTasks(storedTasks);
   }, []);
 
   // add new task
   const addTask = (newTask) => {
-    const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
     const updatedTasks = [...existingTasks, newTask];
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
     setTasks(updatedTasks);
   };
 
+  // edit task
+  const onUpdateTask = (updatedTask) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === updatedTask.id) {
+        return updatedTask;
+      }
+      return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    setTasks(updatedTasks);
+  };
+
+  // delete task
+  const onDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    setTasks(updatedTasks);
+  };
+
+  // Mark task as complete
+  const onMarkComplete = (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, completed: true };
+      }
+      return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks)); 
+    setTasks(updatedTasks);
+  };
+
+  // sort 
+  const sortedTasks = [...tasks].sort((a, b) => {
+    if (a.completed === b.completed) {
+      
+      return 0;
+    }
+   
+    return a.completed ? 1 : -1;
+  });
+
   return (
-    <div className='max-w-[1440px] mx-auto'>
-      <h1 className="text-2xl font-serif font-bold text-center pt-5 pb-3 shadow-lg">ToDo List App</h1>
+    <div className="max-w-[1440px] mx-auto">
+      <h1 className="text-2xl font-serif font-bold text-center pt-5 pb-3 shadow-lg">
+        ToDo List App
+      </h1>
       {/* TaskForm  */}
       <TaskForm onAddTask={addTask} />
-      
+
       {/* Task Cards */}
       <div className="flex justify-between ml-3 mr-5 md:ml-10 md:mr-20">
         <h1 className="text-xl font-serif  font-bold">Task List: </h1>
         <div>
-            <p>Sort</p>
+          <p>Sort</p>
         </div>
-        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-4 md:mx-10">
-        {tasks.map((task) => (
-          <Task key={task.id} task={task} />
+        {sortedTasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onUpdateTask={onUpdateTask}
+            onDeleteTask={onDeleteTask}
+            onMarkComplete={onMarkComplete}
+          />
         ))}
       </div>
     </div>
   );
 }
-
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,34 +1,133 @@
-const Task = ({ task }) => {
+import { useState } from "react";
+
+const Task = ({ task, onUpdateTask, onDeleteTask, onMarkComplete }) => {
+  const [editedTask, setEditedTask] = useState(task);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    onUpdateTask(editedTask);
+    setIsEditing(false);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedTask({
+      ...editedTask,
+      [name]: value,
+    });
+  };
+  const handleDelete = () => {
+    onDeleteTask(task.id);
+  };
+
+  const handleComplete = () => {
+    onMarkComplete(task.id);
+  };
+
   return (
     <div>
       <div
         className={`shadow-lg rounded-lg p-4 mb-4 
-  ${
-    task.completed
-      ? "bg-green-200"
-      : task.priority === "high"
-      ? "bg-red-100"
-      : task.priority === "medium"
-      ? "bg-yellow-50"
-      : "bg-cyan-100"
-  }`}
+          ${
+            task.completed
+              ? "bg-green-200"
+              : task.priority === "high"
+              ? "bg-red-100"
+              : task.priority === "medium"
+              ? "bg-yellow-50"
+              : "bg-cyan-100"
+          }`}
       >
         <h2 className="text-xl font-semibold mb-2">{task.title}</h2>
         <p className="text-gray-600 mb-2">{task.description}</p>
         <p className="text-gray-500 mb-2">Priority: {task.priority}</p>
         <p className="text-gray-500 mb-2">Due Date: {task.dueDate}</p>
         <div className="flex justify-between items-center">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+          <button
+            onClick={handleEdit}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+          >
             Edit
           </button>
-          <button className="bg-red-500 text-white px-4 py-2 rounded-lg">
+          <button
+            onClick={handleDelete}
+            className="bg-red-500 text-white px-4 py-2 rounded-lg"
+          >
             Delete
           </button>
-          <button className="px-4 py-2 rounded-lg text-white bg-green-500">
-            Mark Complete
-          </button>
+          {task.completed ? (
+            <button
+              onClick={handleComplete}
+              className="px-4 py-2 rounded-lg text-white bg-green-500"
+            >
+              Completed
+            </button>
+          ) : (
+            <button
+              onClick={handleComplete}
+              className="px-4 py-2 rounded-lg text-white bg-green-500"
+            >
+              Mark Complete
+            </button>
+          )}
         </div>
       </div>
+
+      {/* Modal for Editing Task */}
+      {isEditing && (
+        <div
+          id="edit-task-modal"
+          className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50"
+        >
+          <div className="bg-white p-6 rounded-lg">
+            <h2 className="text-xl font-semibold mb-2">Edit Task</h2>
+            <input
+              type="text"
+              name="title"
+              value={editedTask.title}
+              onChange={handleChange}
+              placeholder="Title"
+              className="border border-gray-200 px-4 py-2 mb-2 rounded-lg w-full"
+            />
+            <textarea
+              name="description"
+              value={editedTask.description}
+              onChange={handleChange}
+              placeholder="Description"
+              className="border border-gray-200 px-4 py-2 mb-2 rounded-lg w-full h-24"
+            />
+            <select
+              name="priority"
+              value={editedTask.priority}
+              onChange={handleChange}
+              className="border border-gray-200 px-4 py-2 mb-2 rounded-lg w-full"
+            >
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+            </select>
+            <input
+              type="date"
+              name="dueDate"
+              value={editedTask.dueDate}
+              onChange={handleChange}
+              className="border border-gray-200 px-4 py-2 mb-2 rounded-lg w-full"
+            />
+            <div className="flex justify-center">
+              <button
+                onClick={handleSave}
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
